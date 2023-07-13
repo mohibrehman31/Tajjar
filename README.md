c2d5531c96cd4c7d91233fa6c2d0175dd6933203
Mohib Rehman <mohibrehman31@gmail.com>
2023-07-13 19:29:41 +0500

    dsa

--- README.md
+++
@@ -1,4 +0,0 @@
-# TajjarUlBahr
-Steps:
-git pull
-create a new branch and work on that branch
--- src/assets/Images/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Logo } from "./Logo.svg";
-export { default as Interactions } from "./Interactions.svg";
-export { default as FooterLogo } from "./FooterLogo.svg";
-export { default as HomeCurve } from "./HomeCurve.svg";
-export { default as Time } from "./Time.svg";
-export { default as Container } from "./Container.svg";

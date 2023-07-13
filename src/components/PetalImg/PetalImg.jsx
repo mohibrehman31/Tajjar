a7a02ecef7a84e49409723c3df66c389bb212596
Mohib Rehman <mohibrehman31@gmail.com>
2023-07-14 00:16:43 +0500

    Commited

---
+++ src/components/PetalImg/PetalImg.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './PetalImg.scss';
+
+const PetalImg = ({ src, className }) => (
+  <div className={`petal-img ${className}`}>
+    <img src={src} alt="Petal" />
+  </div>
+);
+
+export default PetalImg;
---
+++ src/components/SwiperSlider/Swiper.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "./Swiper.scss";
+import { Autoplay } from "swiper/modules";
+import ValueCard from "src/components/ValueCard/valueCard";
+import CustomerSatisfactionImage from "src/assets/Images/Satisfaction.svg";
+import EfficiencyImage from "src/assets/Images/Efficiency.svg";
+import ImprovementImage from "src/assets/Images/Improvement.svg";
+import IntegrityImage from "src/assets/Images/Integrity.svg";
+
+export default function Swipeslider() {
+  const [setSwiperRef] = useState(null);
+  return (
+    <>
+      <div className="slider">
+        <Swiper
+          onSwiper={setSwiperRef}
+          slidesPerView={3}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          centeredSlides={true}
+          spaceBetween={30}
+          modules={[Autoplay]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <ValueCard
+              img={CustomerSatisfactionImage}
+              caption="Customer Satisfaction"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ValueCard img={EfficiencyImage} caption="Efficiency" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ValueCard
+              img={ImprovementImage}
+              caption="Improvement & Innovation"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ValueCard img={IntegrityImage} caption="Integrity" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </>
+  );
+}

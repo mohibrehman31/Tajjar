6bfb2aa0397a72f17a2afda2c39951a24a92eb50
mohibrehman31 <mohibrehman31@gmail.com>
2023-07-25 23:01:04 +0500

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import "./App.scss";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import User from "./pages/User";
 import "antd/dist/reset.css";
-import { MainLayout } from "src/components/Layout/layout";
+import { MainLayout } from "./components/Layout/Layout";
+import Login from "src/pages/LoginPage/index";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/User/*" element={<User />} />
-          <Route path="/" element={<MainLayout />} />
+          <Route path="/" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>

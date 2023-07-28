b765101ef26f5aed1a63808d205fd52d0e887964
mohibrehman31 <mohibrehman31@gmail.com>
2023-07-28 16:49:44 +0500

    Updated

--- src/pages/ShipManagement/gridData.tsx
+++ src/pages/ShipManagement/gridData.tsx
@@ -1,60 +1,61 @@
 const cards = [
-    {
-      img: 'crewing',
-      title: 'crewing',
-      description: 'Expert Crew Management for Smooth Operations at Sea'
-    },
-    {
-      img: 'insurance',
-      title: 'Insurance',
-      description: 'Protecting Your Assets with Comprehensive Marine Insurance'
-    },
-    {
-      img: 'certification',
-      title: 'Certificaton',
-      description: 'Ensuring Compliance and Quality with Ships Certification'
-    },
-    {
-      img: 'maintenance',
-      title: 'Maintenance',
-      description: 'Efficient Vessel Maintenance to Prolong Ship Lifespan'
-    },
-  
-    {
-      img: 'communication',
-      title: 'Communication',
-      description: 'Seamless Connectivity for Efficient Maritime Operations'
-    },
-    {
-      img: 'technical_services',
-      title: 'Technical Services',
-      description: 'Specialized Technical Support for Optimal Ship Performance'
-    },
-    {
-      img: 'offshore_services',
-      title: 'Offshore Services',
-      description: 'Offshore Solutions Tailored to Your Unique Requirements'
-    },
-    {
-      img: 'consultancy',
-      title: 'Marine Consultancy',
-      description: 'Guiding Your Maritime Endeavors with Expert Consultancy'
-    },
-    {
-      img: 'management',
-      title: 'Comm Management',
-      description: 'Streamlining Communication Systems for Effective Collaboration'
-    },
-    {
-      img: 'brokerage',
-      title: 'Ship Brokerage',
-      description: 'Connecting Buyers and Sellers in the Maritime Market'
-    },
-    {
-      img: 'inspection',
-      title: 'Inspection & Services',
-      description: 'Thorough Inspections and Surveys for Vessel Integrity'
-    }
-  ]
+  {
+    img: "crewing",
+    title: "crewing",
+    description: "Expert Crew Management for Smooth Operations at Sea",
+  },
+  {
+    img: "insurance",
+    title: "Insurance",
+    description: "Protecting Your Assets with Comprehensive Marine Insurance",
+  },
+  {
+    img: "certification",
+    title: "Certificaton",
+    description: "Ensuring Compliance and Quality with Ships Certification",
+  },
+  {
+    img: "maintenance",
+    title: "Maintenance",
+    description: "Efficient Vessel Maintenance to Prolong Ship Lifespan",
+  },
 
-  export default cards;
+  {
+    img: "communication",
+    title: "Communication",
+    description: "Seamless Connectivity for Efficient Maritime Operations",
+  },
+  {
+    img: "technical_services",
+    title: "Technical Services",
+    description: "Specialized Technical Support for Optimal Ship Performance",
+  },
+  {
+    img: "offshore_services",
+    title: "Offshore Services",
+    description: "Offshore Solutions Tailored to Your Unique Requirements",
+  },
+  {
+    img: "consultancy",
+    title: "Marine Consultancy",
+    description: "Guiding Your Maritime Endeavors with Expert Consultancy",
+  },
+  {
+    img: "management",
+    title: "Comm Management",
+    description:
+      "Streamlining Communication Systems for Effective Collaboration",
+  },
+  {
+    img: "brokerage",
+    title: "Ship Brokerage",
+    description: "Connecting Buyers and Sellers in the Maritime Market",
+  },
+  {
+    img: "inspection",
+    title: "Inspection & Services",
+    description: "Thorough Inspections and Surveys for Vessel Integrity",
+  },
+];
+
+export default cards;
--- src/pages/ShipManagement/index.tsx
+++ src/pages/ShipManagement/index.tsx
@@ -7,7 +7,6 @@ import { shipManagement } from "src/assets/Images";
 import ShipManagementCard from "src/components/ShipManagementCard";
 import cards from "./gridData";
 
-
 export const ShipManagement = () => (
   <Space
     direction="vertical"
@@ -27,64 +26,176 @@ export const ShipManagement = () => (
         <Header />
 
         <div className="ship-management-content">
-            <div className="ship-management-head">
-                
-                <div>
-                    <div className="column-1">
-                        <text style={{color: '#242A43', fontSize: '20px', fontWeight: '600', lineHeight: '60px'}}>Ship Management</text>
-                        <br/>
-                        <span style={{color: '#00DA9F', fontSize: '40px',lineHeight: '50px'}}>Navigating </span>
-                        <text style={{color: "#2D3194", fontSize: '40px',lineHeight: '50px'}}>Excellence:</text>
-                        <br/>
-                        <text style={{color: "#242A43", fontSize: '40px',fontWeight: '300', lineHeight: '50px'}}>Explore Exceptional Ship</text>
-                        <br/>
-                        <text style={{color: "#242A43", fontSize: '40px',fontWeight: '300',lineHeight: '50px'}}>Management Services</text>
-                        <p style={{color: "#424242", fontSize: '15px',fontWeight: '300', marginTop: '10px', lineHeight: '20px'}}>TAJJAR-UL-BAHR's primary focus is to deliver cost-effective and top-notch ship management services, ensuring exceptional performance while adhering to Class rules and shipping regulations. 
-                            The company works closely with the Principal to fulfill their specific needs, employing efficient performance monitoring and cost management. 
-                            The utmost priority is given to the Principal's interests, all while maintaining compliance with the Management Agreement.</p>
-                    </div>
-                </div>
-
-                <div>
-                    <img src={shipManagement} className="ship-management-head-image"/>
-                </div>
-
+          <div className="ship-management-head">
+            <div>
+              <div className="column-1">
+                <text
+                  style={{
+                    color: "#242A43",
+                    fontSize: "20px",
+                    fontWeight: "600",
+                    lineHeight: "60px",
+                  }}
+                >
+                  Ship Management
+                </text>
+                <br />
+                <span
+                  style={{
+                    color: "#00DA9F",
+                    fontSize: "40px",
+                    lineHeight: "50px",
+                  }}
+                >
+                  Navigating{" "}
+                </span>
+                <text
+                  style={{
+                    color: "#2D3194",
+                    fontSize: "40px",
+                    lineHeight: "50px",
+                  }}
+                >
+                  Excellence:
+                </text>
+                <br />
+                <text
+                  style={{
+                    color: "#242A43",
+                    fontSize: "40px",
+                    fontWeight: "300",
+                    lineHeight: "50px",
+                  }}
+                >
+                  Explore Exceptional Ship
+                </text>
+                <br />
+                <text
+                  style={{
+                    color: "#242A43",
+                    fontSize: "40px",
+                    fontWeight: "300",
+                    lineHeight: "50px",
+                  }}
+                >
+                  Management Services
+                </text>
+                <p
+                  style={{
+                    color: "#424242",
+                    fontSize: "15px",
+                    fontWeight: "300",
+                    marginTop: "10px",
+                    lineHeight: "20px",
+                  }}
+                >
+                  TAJJAR-UL-BAHR's primary focus is to deliver cost-effective
+                  and top-notch ship management services, ensuring exceptional
+                  performance while adhering to Class rules and shipping
+                  regulations. The company works closely with the Principal to
+                  fulfill their specific needs, employing efficient performance
+                  monitoring and cost management. The utmost priority is given
+                  to the Principal's interests, all while maintaining compliance
+                  with the Management Agreement.
+                </p>
+              </div>
             </div>
 
-            <div className="ship-management-body">
-              <div className="row-1">
-                  <div style={{width: '50%', alignItems: 'start'}}>
-                    <text style={{color: "#0340AD", fontSize: '20px',fontWeight: '600',lineHeight: '60px'}}>What's Included?</text>
-                    <br/>
-                    <text style={{color: "black", fontSize: '35px',fontWeight: '400',lineHeight: '45px'}}>Unleashing Our Full Ship Management Services!</text>
-                  </div>
+            <div>
+              <img
+                src={shipManagement}
+                className="ship-management-head-image"
+              />
+            </div>
+          </div>
 
-                  <div style={{width: '70%'}}>
-                    <p style={{color: "#424242", fontSize: '15px',fontWeight: '300', lineHeight: '20px'}}>Embark on a journey of operational excellence with our diverse Ship Management Services. From skilled crewing and streamlined maintenance to efficient communication and marine consultancy, we offer comprehensive 
-                      solutions tailored to your logistics needs. With a focus on safety, certifications, and offshore support, we navigate the seas of success together, ensuring smooth sailing for your maritime ventures</p>
-                  </div>
-               </div>
+          <div className="ship-management-body">
+            <div className="row-1">
+              <div style={{ width: "50%", alignItems: "start" }}>
+                <text
+                  style={{
+                    color: "#0340AD",
+                    fontSize: "20px",
+                    fontWeight: "600",
+                    lineHeight: "60px",
+                  }}
+                >
+                  What's Included?
+                </text>
+                <br />
+                <text
+                  style={{
+                    color: "black",
+                    fontSize: "35px",
+                    fontWeight: "400",
+                    lineHeight: "45px",
+                  }}
+                >
+                  Unleashing Our Full Ship Management Services!
+                </text>
+              </div>
 
-              <div className="row-2-1">
-              { cards.slice(0,8).map((card: {img: string, title:string, description: string}, index: number) => (
-                <div key={index}>
+              <div style={{ width: "70%" }}>
+                <p
+                  style={{
+                    color: "#424242",
+                    fontSize: "15px",
+                    fontWeight: "300",
+                    lineHeight: "20px",
+                  }}
+                >
+                  Embark on a journey of operational excellence with our diverse
+                  Ship Management Services. From skilled crewing and streamlined
+                  maintenance to efficient communication and marine consultancy,
+                  we offer comprehensive solutions tailored to your logistics
+                  needs. With a focus on safety, certifications, and offshore
+                  support, we navigate the seas of success together, ensuring
+                  smooth sailing for your maritime ventures
+                </p>
+              </div>
+            </div>
 
-                  <ShipManagementCard data={{img: card.img, title: card.title, description: card.description}} />
-                </div>
-              ))
-              }
+            <div className="row-2-1">
+              {cards
+                .slice(0, 8)
+                .map(
+                  (
+                    card: { img: string; title: string; description: string },
+                    index: number
+                  ) => (
+                    <div key={index}>
+                      <ShipManagementCard
+                        data={{
+                          img: card.img,
+                          title: card.title,
+                          description: card.description,
+                        }}
+                      />
+                    </div>
+                  )
+                )}
             </div>
             <div className="row-2-2">
-              { cards.slice(-3).map((card: {img: string, title:string, description: string}, index: number) => (
-                <div key={index}>
-
-                  <ShipManagementCard data={{img: card.img, title: card.title, description: card.description}} />
-                </div>
-              ))
-              }
+              {cards
+                .slice(-3)
+                .map(
+                  (
+                    card: { img: string; title: string; description: string },
+                    index: number
+                  ) => (
+                    <div key={index}>
+                      <ShipManagementCard
+                        data={{
+                          img: card.img,
+                          title: card.title,
+                          description: card.description,
+                        }}
+                      />
+                    </div>
+                  )
+                )}
             </div>
-
-            </div>
+          </div>
         </div>
         <Footer />
       </div>

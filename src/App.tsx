0bb2403fe210fab6efa1474e0650d54c04e99550
MidhatKarim28 <mdihatkarim28@gmail.com>
2023-07-28 23:38:26 +0500

    About Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import "./App.scss";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import User from "./pages/User";
 import "antd/dist/reset.css";
-import { MainLayout } from "./components/Layout/Layout";
+// import { MainLayout } from "./components/Layout/Layout";
 import { ShipManagement } from "./pages/ShipManagement";
 import { Services } from "./components/Services";
+import { AboutUs } from "./pages/About Us";
 import crewingData from "./Data";
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/User/*" element={<User />} />
-          <Route path="/" element={<MainLayout />} />
+          <Route path="/" element={<AboutUs />} />
           <Route path="/ShipManagement" element={<ShipManagement />} />
           <Route path="/Services" element={<Services data={crewingData} />} />
         </Routes>

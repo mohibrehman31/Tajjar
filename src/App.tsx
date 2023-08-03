d6c40d9136b54d3a7f110891bf3a6b9dadcd5c85
mohibrehman31 <mohibrehman31@gmail.com>
2023-08-03 16:45:13 +0500

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import "./App.scss";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import User from "./pages/User";
 import "antd/dist/reset.css";
-// import { MainLayout } from "./components/Layout/Layout";
 import { ShipManagement } from "./pages/ShipManagement";
 import { Services } from "./components/Services";
-// import { AboutUs } from "./pages/About Us";
 import crewingData from "./Data";
-// import Insurance from "./pages/Insurance";
 import ShipCertification from "./pages/Ship Certification";
+import Insurance from "./pages/Insurance";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/User/*" element={<User />} />
-          <Route path="/" element={<ShipCertification />} />
+          <Route path="/Services/Insurance" element={<Insurance />} />
+          <Route
+            path="/Services/Certification"
+            element={<ShipCertification />}
+          />
           <Route path="/ShipManagement" element={<ShipManagement />} />
           <Route path="/Services" element={<Services data={crewingData} />} />
         </Routes>
